cfc6fccbffe89649a1be05660413f906475debf6
gh0o5t <gh0o5t@protonmail.com>
2021-01-13 20:53:07 +0100

    Modified border pixel

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
  */
 //static char *font = "Monospace:style=regular:size=10:antialias=true:autohint=true";
 static char *font = "Fantasque Sans Mono:size=10:antialias=true:autohint=true";
-static int borderpx = 2;
+static int borderpx = 5;
 
 /*
  * What program is execed by st depends of these precedence rules:
